f13d5d6ddc2d7799a96855231f8f7721d9f85f28
Akhilesh Tiwari <takhil2212@gmail.com>
2026-06-30 22:21:18 +0530

    chore: some package import and file path import

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import authRoute from './routes/auth.routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+//   /api/auth
+app.use('/api/auth', authRoute);
 
 
 export default app;
